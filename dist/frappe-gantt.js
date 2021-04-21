7d91a006859924cd76d7c85ba17c297ed885d241
medmiskini-kostango <mohamed.miskini@kostango.com>
2021-04-21 16:18:01 +0100

    increase number of years

--- dist/frappe-gantt.js
+++ dist/frappe-gantt.js
@@ -1295,9 +1295,9 @@ class Gantt {
             this.gantt_end = date_utils.add(this.gantt_end, 1, 'year');
         } else if (this.view_is(VIEW_MODE.YEAR)) {
             this.gantt_start = new Date(this.gantt_start.getFullYear(), 0, 1);
-            this.gantt_start = date_utils.add(this.gantt_start, -2, 'year');
+            this.gantt_start = date_utils.add(this.gantt_start, -4, 'year');
             this.gantt_end = new Date(this.gantt_end.getFullYear(), 0, 1);
-            this.gantt_end = date_utils.add(this.gantt_end, 2, 'year');
+            this.gantt_end = date_utils.add(this.gantt_end, 4, 'year');
         } else {
             this.gantt_start = date_utils.add(this.gantt_start, -1, 'month');
             this.gantt_end = date_utils.add(this.gantt_end, 1, 'month');
