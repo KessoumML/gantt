008d349afc23ac51cb1b726566258deb631583e1
medmiskini-kostango <mohamed.miskini@kostango.com>
2021-04-21 14:41:47 +0100

    fix task placement in year mode

--- dist/frappe-gantt.js
+++ dist/frappe-gantt.js
@@ -1294,7 +1294,9 @@ class Gantt {
             this.gantt_start = date_utils.start_of(this.gantt_start, 'year');
             this.gantt_end = date_utils.add(this.gantt_end, 1, 'year');
         } else if (this.view_is(VIEW_MODE.YEAR)) {
+            this.gantt_start = new Date(this.gantt_start.getFullYear(), 0, 1);
             this.gantt_start = date_utils.add(this.gantt_start, -2, 'year');
+            this.gantt_end = new Date(this.gantt_end.getFullYear(), 0, 1);
             this.gantt_end = date_utils.add(this.gantt_end, 2, 'year');
         } else {
             this.gantt_start = date_utils.add(this.gantt_start, -1, 'month');
